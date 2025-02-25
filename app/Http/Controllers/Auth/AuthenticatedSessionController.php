f6ef4ca652967463f7052e363cc878725a9a8dd9
Hamza <hamzacoude@gmail.com>
2025-02-25 12:29:25 +0000

    Fix typo in admin dashboard redirect URL

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -31,7 +31,7 @@ class AuthenticatedSessionController extends Controller
 
         if($request->user()->usertype ==='admin'){
 
-           return redirect('admin/dashbord');
+            return redirect('admin/dashbord');
 
         }
 
