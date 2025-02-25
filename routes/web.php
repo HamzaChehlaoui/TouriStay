e03499b525b30b285b39f50abd77317862bb4d27
Hamza <hamzacoude@gmail.com>
2025-02-25 11:55:35 +0000

    Add HomeController, migration for usertype column, and admin dashboard view

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -33,3 +34,5 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('admin/dashboard', [HomeController::class,'index']);
